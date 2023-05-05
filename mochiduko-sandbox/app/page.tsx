519b62661920c796642728227df48cc573dea33b
motttey <motitago@gmail.com>
2023-05-05 21:45:22 +0900

    modify: fetch using swr method

--- mochiduko-sandbox/app/page.tsx
+++ mochiduko-sandbox/app/page.tsx
@@ -1,37 +1,47 @@
 'use client'
-import Image from 'next/image'
+
+import useSWR from 'swr'
 import styles from './page.module.css'
-import { useEffect, useState } from 'react'
+import { ReactNode, useState } from 'react'
+
+const fetcher = () => fetch('/api/mochiduko').then(res => res.json())
 
 export default function Home() {
-  const [illustList, setIllustList]: any = useState([])
-  useEffect(() => {
-    const fetchIllustList = async () => {
-      const response = await fetch('/api/mochiduko')
-      const data: any = await response.json()
-      setIllustList(data.illusts)
-    }
-    fetchIllustList()
-  },[])
+  const { data, error } = useSWR('/api/user', fetcher)
+
+  async function fetchIllustList(): Promise<ReactNode>{
+  
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+    const illusts = data?.illusts ?? [];
+
+    return <div className={styles.grid}>
+      {illusts.map((illust: any, index: number) => (
+        <a
+          href={illust.url}
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+          key={index}
+        >
+          <h2>
+            {illust.title}
+          </h2>
+          <p>{illust.date}</p>
+        </a>
+      ))}
+    </div>
+  }
+
+  const [element, setElement] = useState<ReactNode>(null)
+
+  fetchIllustList().then((illustList) => {
+    setElement(illustList);
+  });
 
   return (
     <main className={styles.main}>
-      <div className={styles.grid}>
-        {illustList.map((illust: any, index: number) => (
-          <a
-            href={illust.url}
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-            key={index}
-          >
-            <h2>
-              {illust.title}
-            </h2>
-            <p>{illust.date}</p>
-          </a>
-        ))}
-      </div>
+      {element}
     </main>
   )
 }
