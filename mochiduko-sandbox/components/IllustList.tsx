aedcc9fa7514aea07cde320bfed050054628a810
motttey <motitago@gmail.com>
2023-05-14 20:26:04 +0900

    modify: show image on list

--- mochiduko-sandbox/components/IllustList.tsx
+++ mochiduko-sandbox/components/IllustList.tsx
@@ -1,5 +1,7 @@
 'use client'
 import useSWR from 'swr'
+import Image from 'next/image';
+
 import styles from '@/app/page.module.css'
 import { Illust } from '@/types/api'
 
@@ -11,6 +13,8 @@ const IllustList: React.FC = () => {
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
   const illusts: Array<Illust> = data?.illusts ?? [];
+
+  const fetchUrl = (id: string) => `http://embed.pixiv.net/decorate.php?illust_id=${id || ''}`
 
   return <div className={styles.grid}>
     {illusts.map((illust: Illust, index: number) => (
@@ -25,6 +29,14 @@ const IllustList: React.FC = () => {
           {illust.title}
         </h2>
         <p>{illust.date}</p>
+        <Image
+            src={fetchUrl(illust.id.toString())}
+            alt={illust.title}
+            width={200}
+            height={100}
+            loading='lazy'
+            className="white--text align-end"
+         />
       </a>
     ))}
   </div>
