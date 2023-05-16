3dd6be44aea031c4f237ecd9493ffc74baebaa9f
motttey <motitago@gmail.com>
2023-05-16 21:01:38 +0900

    modify: filter contents by query string

--- mochiduko-sandbox/components/IllustList.tsx
+++ mochiduko-sandbox/components/IllustList.tsx
@@ -1,18 +1,34 @@
 'use client'
 import useSWR from 'swr'
 import Image from 'next/image';
+import { useSearchParams } from 'next/navigation';
 
 import styles from '@/app/page.module.css'
 import { Illust } from '@/types/api'
+import { useState, useEffect, useMemo } from 'react';
 
 const fetcher = () => fetch('/api/mochiduko').then(res => res.json())
-
 const IllustList: React.FC = () => {
-    const { data, error } = useSWR('/api/user', fetcher)
+  const { data, error } = useSWR('/api/user', fetcher)
+  const [illusts, setIllusts] = useState([] as  Array<Illust>)
+  const router = useSearchParams();
+  const query = router.get("title");
+
+  useMemo(() => {
+    const fetchedIllust: Array<Illust> = data?.illusts ?? [];
+    setIllusts(fetchedIllust)
+  }, [data?.illusts])
+
+  useEffect(() => {
+    if (query) {
+      console.log(query);
+      const filterdIllusts = illusts.filter((illust: Illust) => illust.title.includes(query))
+      setIllusts(filterdIllusts)
+    }
+  },[illusts, query]);
 
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
-  const illusts: Array<Illust> = data?.illusts ?? [];
 
   const fetchUrl = (id: string) => `http://embed.pixiv.net/decorate.php?illust_id=${id || ''}`
 
