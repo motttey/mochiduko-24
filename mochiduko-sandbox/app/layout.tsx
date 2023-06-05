8103a26ab304d3f2f0c4c27dd3c7bb16ef5f46dd
motttey <motitago@gmail.com>
2023-06-05 22:30:12 +0900

    fix: meta tag

--- mochiduko-sandbox/app/layout.tsx
+++ mochiduko-sandbox/app/layout.tsx
@@ -1,7 +1,10 @@
-import Header from './api/mochiduko/Header'
 import './globals.css'
 import { Inter, Noto_Sans_JP } from 'next/font/google'
 import Head from 'next/head';
+
+
+import Header from './api/mochiduko/Header'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +16,15 @@ const NotoSansJP = Noto_Sans_JP({
 
 export const metadata = {
   title: 'モチヅ庫\'24',
-  description: 'モチヅ庫\'24',
+  description: '望月 田吾作 (もちづき たごさく)が描いた、ドラえもんや藤子不二雄作品などのイラストや漫画を掲載しているサイトです.',
+  keywords: '望月,望月田吾作,ドラえもん,Doraemon,Fujiko Fujio,藤子不二雄,藤子・F・不二雄,イラスト,ドラえもん イラスト',
+  author: 'Tagosaku Mochiduki',
+  og_site_name: 'モチヅ庫\'24',
+  og_type: 'website',
+  og_url: 'http://motttey.github.io/mochiduko-20',
+  og_image: 'https://motttey.github.io/mochiduko-20/drawer-bg.webp',
+  twitter_card: 'summary_large_image',
+  twitter_site: '@mt_tg'
 }
 
 export default function RootLayout({
@@ -25,10 +36,61 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <title>{metadata.title}</title>
+        <meta charSet="utf-8"/>
         <meta
           name="description"
           content={metadata.description}
           key="desc"
+        />
+        <meta
+          name="keywords"
+          content={metadata.keywords}
+          key="keywords"
+        />
+        <meta
+          name="og:site_name"
+          content={metadata.og_site_name}
+          key="og:site_name"
+        />
+        <meta
+          name="og:type"
+          content={metadata.og_type}
+          key="og:type"
+        />
+        <meta
+          name="og:type"
+          content={metadata.og_type}
+          key="og:type"
+        />
+        <meta
+          name="og:url"
+          content={metadata.og_url}
+          key="og:url"
+        />
+        <meta
+          name="og:image"
+          content={metadata.og_image}
+          key="og:image"
+        />
+        <meta
+          name="twitter:card"
+          content={metadata.twitter_card}
+          key="twitter:card"
+        />
+        <meta
+          name="twitter:site"
+          content={metadata.twitter_site}
+          key="twitter:site"
+        />
+        <meta
+          name="twitter:title"
+          content={metadata.title}
+          key="twitter:title"
+        />
+        <meta
+          name="twitter:description"
+          content={metadata.description}
+          key="twitter:description"
         />
       </Head>
       <body className={`${NotoSansJP.className} ${inter.className}`}>
