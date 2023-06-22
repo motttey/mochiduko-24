19a26340b48cd297c579959b8b58e10f9b0cea85
motttey <motitago@gmail.com>
2023-06-22 22:30:55 +0900

    fix: add locale

--- mochiduko-sandbox/next.config.js
+++ mochiduko-sandbox/next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  i18n: {
+    locales: ['ja'],
+    defaultLocale: 'ja',
+  },
   images: {
     domains: ['embed.pixiv.net'],
   },
