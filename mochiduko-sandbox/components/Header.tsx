67dac85aa2b16214e05a49f597be345a23c1d2e2
motttey <motitago@gmail.com>
2023-06-29 22:08:11 +0900

    fix: Header

--- mochiduko-sandbox/components/Header.tsx
+++ mochiduko-sandbox/components/Header.tsx
@@ -1,18 +1,18 @@
 import Link from "next/link";
 
-const Header: React.FC = () => {
+const SiteHeader: React.FC = () => {
   const title = 'Mochiduko 2024'
   return (
-    <div className="bg-white lg:pb-6">
-      <div className="max-w-screen-2xl px-2 md:px-4 mx-auto">
-        <header className="flex justify-between items-center py-4">
+    <header>
+      <div className="bg-white lg:pb-6">
+        <div className="max-w-screen-2xl px-2 md:px-4 mx-auto">
           <Link href="/" className="header-link" aria-label="logo">
             {title}
           </Link>
-        </header>
+        </div >
       </div >
-    </div >
+    </header>
   )
 };
 
-export default Header;
+export default SiteHeader;
