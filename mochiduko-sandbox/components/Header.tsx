9541991c2be6c531213aa4d0a12c556f15680dd8
motttey <motitago@gmail.com>
2023-09-06 21:22:36 +0900

    fix: set header component

--- mochiduko-sandbox/components/Header.tsx
+++ mochiduko-sandbox/components/Header.tsx
@@ -1,9 +1,11 @@
+"use client"
 import Link from "next/link";
+import { Header } from "semantic-ui-react";
 
 const SiteHeader: React.FC = () => {
   const title = 'Mochiduko 2024'
   return (
-    <header>
+    <Header>
       <div className="bg-white lg:pb-6">
         <div className="max-w-screen-2xl px-2 md:px-4 mx-auto">
           <Link href="/" className="header-link" aria-label="logo">
@@ -11,7 +13,7 @@ const SiteHeader: React.FC = () => {
           </Link>
         </div >
       </div >
-    </header>
+    </Header>
   )
 };
 
