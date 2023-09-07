9d62f0dad9617d887147a91a36e91029fc5c65ba
motttey <motitago@gmail.com>
2023-09-07 22:07:35 +0900

    modify: header styles

--- mochiduko-sandbox/components/Header.tsx
+++ mochiduko-sandbox/components/Header.tsx
@@ -1,18 +1,16 @@
 "use client"
 import Link from "next/link";
-import { Header } from "semantic-ui-react";
+import { Divider, Header, Image } from "semantic-ui-react";
 
 const SiteHeader: React.FC = () => {
   const title = 'Mochiduko 2024'
   return (
     <Header as='h1'>
-      <div className="bg-white lg:pb-6">
-        <div className="max-w-screen-2xl px-2 md:px-4 mx-auto">
-          <Link href="/" className="header-link" aria-label="logo">
-            {title}
-          </Link>
-        </div >
-      </div >
+      <Image spaced='right' circular alt="header-img" src='https://motttey.github.io/doraemon-namecard.webp' />  
+      <Link href="/" className="header-link" aria-label="logo">
+        {title}
+      </Link>
+      <Divider horizontal>◇</Divider>
     </Header>
   )
 };
