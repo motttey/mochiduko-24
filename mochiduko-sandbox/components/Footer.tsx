c12e3ad6808c72f7680a4784449af42a7736c421
motttey <motitago@gmail.com>
2023-09-09 19:00:56 +0900

    modify: footer styles

--- mochiduko-sandbox/components/Footer.tsx
+++ mochiduko-sandbox/components/Footer.tsx
@@ -1,9 +1,12 @@
+"use client"
 import Link from "next/link";
+import { Divider } from "semantic-ui-react";
 
 const SiteFooter: React.FC = () => {
   const title = `© ${new Date().getFullYear()} Tagosaku Mochiduki`
   return (
     <footer>
+        <Divider horizontal>◇</Divider>
         <div className="max-w-screen-2xl px-2 md:px-4 mx-auto">
             <Link href="/" className="footer-link" aria-label="logo">
             {title}
