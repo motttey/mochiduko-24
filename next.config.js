b21ad4c153e54171343aecc8af64fce8d1a5e54a
motttey <motitago@gmail.com>
2023-09-10 20:21:55 +0900

    Revert "modify: use export"
    This reverts commit 5dce019405ef2dfcfd2530df85d3eabb16c06e6c.

--- next.config.js
+++ next.config.js
@@ -2,17 +2,14 @@
 const repositoryName = process.env.REPOSITORY_NAME ? "/" + process.env.REPOSITORY_NAME : "";
 
 const nextConfig = {
-  output: 'export',
   experimental: {
     appDir: true,
     serverActions: true,
   },
-  /*
   i18n: {
     locales: ['ja'],
     defaultLocale: 'ja',
   },
-  */
   images: {
     domains: ['embed.pixiv.net'],
   },
