ac08279276ed260626750daf78dc708ffff49895
motttey <motitago@gmail.com>
2023-09-18 20:43:03 +0900

    fix: deploy mode

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const getData = async () => {
   // 1時間ごとにprefetchする
   let res: Array<Illust> = new Array<Illust>();
   
-  if (process.env.DEPLOY_MODE !== "SSR") { 
+  if (process.env.DEPLOY_MODE === "SSR") { 
     res = await fetch('https://mochiduko-api.netlify.app/each_illusts.json',
       { next: { revalidate: 60 * 60 } }
     )
@@ -33,7 +33,7 @@ function SearchBarFallback() {
 export default async function Page() {
   let initialContentsList: Array<Illust> = new Array<Illust>();
   
-  if (process.env.DEPLOY_MODE !== "SSR") { 
+  if (process.env.DEPLOY_MODE === "SSR") { 
     const res: any = await getData()
     initialContentsList = res.data;
   }
