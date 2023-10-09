b30193ef6b6afb250f6a6e67cf3d2475e946d4c2
motttey <motitago@gmail.com>
2023-10-09 17:36:40 +0900

    fix: set header sticky

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,16 +6,18 @@ import styles from '@/app/page.module.css'
 const SiteHeader: React.FC = () => {
   const title = 'Mochiduko 2024'
   return (
-    <Header as='h1' dividing>
-      <Image 
-        spaced='right'
-        className={styles.hex}
-        src='https://motttey.github.io/doraemon-namecard.webp'
-      />  
-      <Link href="/" className="header-link" aria-label="logo">
-        {title}
-      </Link>
-    </Header>
+    <header>
+      <Header as='h1' dividing>
+        <Image 
+          spaced='right'
+          className={styles.hex}
+          src='https://motttey.github.io/doraemon-namecard.webp'
+        />  
+        <Link href="/" className="header-link" aria-label="logo">
+          {title}
+        </Link>
+      </Header>
+    </header>
   )
 };
 
