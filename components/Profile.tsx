42662c956724f6ba2d129862a4b8e49be5220d0e
motttey <motitago@gmail.com>
2023-12-15 22:20:16 +0900

    modify: card styles

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,31 +1,39 @@
 'use client'
-import { Image, Card, Grid, Blockquote } from '@mantine/core';
+import { Image, Card, Grid, Blockquote, Text } from '@mantine/core';
 
 const Profile: React.FC<any> = (_: any) => {
   return (
     <div style={{
         maxWidth: "100vw",
-        minWidth: "80vw"
+        minWidth: "80vw",
+        pointerEvents: "none"
     }}>
-        <Grid my="base" >
+        <Grid my="5" >
             <Grid.Col>
                 <h2>Profile</h2>
             </Grid.Col>
         </Grid>
-        <Grid my="base" >
+        <Grid my="10">
             <Grid.Col span={6}>
-                <Card shadow="sm" padding="md" radius="20">
+                <Card shadow="sm" padding="md" radius="0">
                     <Card.Section>
                         <Image
                             src='https://motttey.github.io/doraemon-namecard.webp'
                             height={360}
+                            fit="cover"
                             alt="Tagosaku Mochiduki Profile"
                         />
                     </Card.Section>
                 </Card>
             </Grid.Col>
             <Grid.Col span={6}>
-                <Card shadow="sm" padding="md" radius="20">
+                <Card shadow="sm" padding="md" radius="0">
+                    <Card.Section>
+                        <Text my={4}>
+                            お仕事のご依頼や感想は、 motitago(at)gmail.com までよろしくお願いします。
+                            もしイラストなど気に入って頂けた場合には、ほしいものリストから何か送っていただけるとすごい喜びます。
+                        </Text>
+                    </Card.Section>
                     <Card.Section>
                         <Blockquote cite="Tagosaku Mochiduki">
                             He is a Hyper Doraemon Creator
