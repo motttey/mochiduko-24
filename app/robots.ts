7a66fb2cd7d02da398420afbc204eb5a4cec1de1
motttey <motitago@gmail.com>
2024-03-23 18:05:43 +0900

    modify: robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,9 +2,16 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "GPTBot",
-      disallow: "/",
-    },
+    rules: [
+      {
+        userAgent: "GPTBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: ["/"],
+      },
+    ],
+    sitemap: "https://motttey.github.io/mochiduko-24/sitemap.xml",
   };
 }
