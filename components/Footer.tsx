f656bcf13dfceb0fcb3ca69f45822947b441a80a
motttey <motitago@gmail.com>
2024-04-02 00:02:17 +0900

    fix: footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,10 +9,15 @@ const SiteFooter: React.FC = () => {
     <footer
       style={{
         minWidth: "100vw",
+        fontWeight: "700",
+        margin: "10px 0px",
+        color: "#868e96",
       }}
     >
       <Container size="md">
-        <Divider className={styles.commonDivider}></Divider>
+        <Divider className={styles.commonDivider} style={{
+          marginBottom: "10px"
+        }}></Divider>
         <Link href="/" className="footer-link" aria-label="logo">
           {title}
         </Link>
