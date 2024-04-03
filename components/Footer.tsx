0f58230593f0b961832588e3d2f530dc53a07daa
motttey <motitago@gmail.com>
2024-04-04 00:32:33 +0900

    fix: detailed style, format

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,9 +15,12 @@ const SiteFooter: React.FC = () => {
       }}
     >
       <Container size="md">
-        <Divider className={styles.commonDivider} style={{
-          marginBottom: "10px"
-        }}></Divider>
+        <Divider
+          className={styles.commonDivider}
+          style={{
+            marginBottom: "10px",
+          }}
+        ></Divider>
         <Link href="/" className="footer-link" aria-label="logo">
           {title}
         </Link>
