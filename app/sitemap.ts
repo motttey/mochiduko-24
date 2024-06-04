472cb866203dba1219cd62f68ee3a7eab99f4c19
motttey <motitago@gmail.com>
2024-06-04 21:49:20 +0900

    fix: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,10 +3,22 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://motttey.github.io/mochiduko-24",
+      url: "https://motttey.github.io",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
+    },
+    {
+      url: "https://motttey.github.io/dialy",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://motttey.github.io/mochiduko-24",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
     },
     {
       url: "https://motttey.github.io/mochiduko-20",
