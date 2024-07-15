ac15aa854f3a07729bac8e204fc267f37d7c981c
motttey <motitago@gmail.com>
2024-07-15 14:15:11 +0900

    add: tree/page.tsx

---
+++ app/three/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+import styles from "@/app/page.module.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+
+// https://github.com/pmndrs/react-three-fiber
+function Box(props: any) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((_state, delta) => {
+    // Added null check and rotation property access
+    if (ref.current && (ref.current as any).rotation) {
+      (ref.current as any).rotation.x += delta;
+    }
+  });
+
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
+export default function Page() {
+  return (
+    <>
+      <main className={styles.main} id="mainLayout">
+        <Canvas>
+          <ambientLight intensity={Math.PI / 2} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <pointLight
+            position={[-10, -10, -10]}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
+      </main>
+    </>
+  );
+}
