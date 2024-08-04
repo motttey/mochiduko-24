072f09ee7973b5fe0ad7eeba17bd195c8097df2d
motttey <motitago@gmail.com>
2024-08-04 17:52:07 +0900

    fix: indent

--- app/three/page.tsx
+++ app/three/page.tsx
@@ -47,37 +47,35 @@ function Box(props: any) {
 
 export default function Page() {
   return (
-    <>
-      <main className={styles.main} id="mainLayout">
-        <div style={{ width: "50vw", height: "75vh" }}>
-          <Canvas>
-            <ambientLight intensity={Math.PI / 2} />
-            <spotLight
-              position={[POSITION_MAX, POSITION_MAX, POSITION_MAX]}
-              angle={0.15}
-              penumbra={1}
-              decay={0}
-              intensity={Math.PI}
-            />
-            <pointLight
-              position={[-POSITION_MAX, -POSITION_MAX, -POSITION_MAX]}
-              decay={0}
-              intensity={Math.PI}
-            />
-            {Array(BOX_NUM)
-              .fill(0)
-              .map((index: number) => {
-                const x = (Math.random() * POSITION_MAX) / 2 - POSITION_MAX / 2;
-                const y = (Math.random() * POSITION_MAX) / 2;
-                const z = (Math.random() * POSITION_MAX) / 3 - POSITION_MAX / 3;
+    <main className={styles.main} id="mainLayout">
+      <div style={{ width: "50vw", height: "75vh" }}>
+        <Canvas>
+          <ambientLight intensity={Math.PI / 2} />
+          <spotLight
+            position={[POSITION_MAX, POSITION_MAX, POSITION_MAX]}
+            angle={0.15}
+            penumbra={1}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <pointLight
+            position={[-POSITION_MAX, -POSITION_MAX, -POSITION_MAX]}
+            decay={0}
+            intensity={Math.PI}
+          />
+          {Array(BOX_NUM)
+            .fill(0)
+            .map((index: number) => {
+              const x = (Math.random() * POSITION_MAX) / 2 - POSITION_MAX / 2;
+              const y = (Math.random() * POSITION_MAX) / 2;
+              const z = (Math.random() * POSITION_MAX) / 3 - POSITION_MAX / 3;
 
-                return <Box key={index} position={[x, y, z]} />;
-              })}
-            ;
-            <OrbitControls />
-          </Canvas>
-        </div>
-      </main>
-    </>
+              return <Box key={index} position={[x, y, z]} />;
+            })}
+          ;
+          <OrbitControls />
+        </Canvas>
+      </div>
+    </main>
   );
 }
