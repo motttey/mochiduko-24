76355e6179a2547f2377eda588bb0fed0cb71476
motttey <motitago@gmail.com>
2024-08-27 20:54:56 +0900

    install: next-pwa

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 // https://github.com/actions/upload-pages-artifact/issues/74
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({
   output: "export",
   images: {
     domains: ["embed.pixiv.net"],
@@ -10,6 +17,6 @@ const nextConfig = {
   // デプロイ先が /mochiduko-24などになる場合はprefixPathをいじる
   assetPrefix: "",
   basePath: "",
-};
+});
 
 module.exports = nextConfig;
