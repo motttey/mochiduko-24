2f87019b7a4e21af8da9050ad70e5061a6e706e5
motttey <motitago@gmail.com>
2024-09-01 12:36:37 +0900

    modify: ambient style

--- app/three/page.tsx
+++ app/three/page.tsx
@@ -7,7 +7,7 @@ import { useMemo, useRef, useState } from "react";
 import { Color, Mesh, TextureLoader } from "three";
 
 const POSITION_MAX = 10;
-const BOX_NUM = 15;
+const BOX_NUM = 20;
 
 function Box(props: any) {
   const ref = useRef<Mesh>(null);
@@ -17,8 +17,9 @@ function Box(props: any) {
   // 定期的に移動し, 画面外に出たらリセットする
   const initialPosition = useMemo(() => props.position, [props.position]);
 
+  // X方向のランダムな回転角度（-15度から15度の間）
   useFrame((_state, delta) => {
-    if (ref.current) {
+    if (ref?.current) {
       ref.current.position.y -= delta * 2;
       if (ref.current.position.y < -POSITION_MAX / 2) {
         ref.current.position.y = POSITION_MAX / 2;
@@ -28,12 +29,14 @@ function Box(props: any) {
 
   const loader = new TextureLoader();
   const texture = loader.load("/dorayaki.png");
-
+  const scale = 1 * Math.random() + 0.5;
+  const rotation = Math.random() * 10;
   return (
     <mesh
       {...props}
       ref={ref}
       position={initialPosition}
+      rotation={[rotation, rotation, 0]}
       scale={clicked ? 1.5 : 1}
       onClick={() => click(!clicked)}
       onPointerOver={() => hover(true)}
@@ -44,7 +47,7 @@ function Box(props: any) {
       */}
       <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
       {/* Add a Sprite */}
-      <sprite scale={[1, 1, 1]} position={[0, 1, 0]}>
+      <sprite scale={[scale, scale, 1]} position={[0, 1, 0]}>
         <spriteMaterial
           attach="material"
           color={new Color(0xffffff)}
@@ -58,12 +61,12 @@ function Box(props: any) {
 }
 
 export default function Page() {
-  const maxinTexture = new TextureLoader().load("/icon512_rounded.png");
+  const maxinTexture = new TextureLoader().load("/dorayakidaisuki.png");
 
   return (
     <main className={styles.main} id="mainLayout">
       <div style={{ width: "100vw", height: "75vh" }}>
-        <Canvas>
+        <Canvas linear flat>
           <ambientLight color={new Color(0xffffff)} intensity={1} />
           <spotLight
             position={[POSITION_MAX, POSITION_MAX, POSITION_MAX]}
@@ -88,14 +91,8 @@ export default function Page() {
             })}
           ;
           <OrbitControls />
-          <mesh
-            position={[
-              1,
-              1,
-              -(Math.random() * POSITION_MAX) / 3 - POSITION_MAX / 3,
-            ]}
-          >
-            <sprite scale={[10, 10, 1]} position={[0, 0, 1]}>
+          <mesh position={[1, 1, -POSITION_MAX / 3]}>
+            <sprite scale={[7.5, 7.5, 0]} position={[0, 0, 1]}>
               <spriteMaterial
                 attach="material"
                 color={new Color(0xffffff)}
