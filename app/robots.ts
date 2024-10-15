75d9b1c894e97aff5803820236afab5061fea853
motttey <motitago@gmail.com>
2024-10-15 22:03:14 +0900

    fix: update robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -4,6 +4,16 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
+        // 全てのクローラ（Googlebot以外含む）に対して特定階層を許可
+        userAgent: "*",
+        allow: ["/", "/dialy"],
+      },
+      {
+        // Googlebotに対して全てのパスを許可
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+      {
         userAgent: "GPTBot",
         disallow: ["/", "/dialy"],
       },
