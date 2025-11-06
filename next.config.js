3532b07d02141fa19fa08a33a3d6e0cd08cb36e3
motttey <motitago@gmail.com>
2025-11-06 19:24:38 +0900

    fix:: migrate next, remove next-pwa

--- next.config.js
+++ next.config.js
@@ -2,18 +2,12 @@
 // https://github.com/actions/upload-pages-artifact/issues/74
 
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const nextConfig = withPWA({
+const nextConfig = {
   output: "export",
   images: {
     remotePatterns: [
@@ -27,7 +21,7 @@ const nextConfig = withPWA({
   // デプロイ先が /mochiduko-24などになる場合はprefixPathをいじる
   assetPrefix: "",
   basePath: "",
-});
+};
 
 const nextConfigWithBundleVisualizer = withBundleAnalyzer(nextConfig);
 
