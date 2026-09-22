6b7d0905d5ddb4c9bbd1b79be10827bb6a03fa61
motttey <motitago@gmail.com>
2026-09-22 18:47:37 +0900

    chore: use eslint flat config

--- app/plateau/PlateauTilesetTransformContext.tsx
+++ app/plateau/PlateauTilesetTransformContext.tsx
@@ -4,6 +4,5 @@ import { createContext } from "react";
 import { Vector3 } from "three";
 
 export const PlateauTilesetTransformContext = createContext({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setCenter: (_center: Vector3): void => {},
 });
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,25 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import js from "@eslint/js";
-import nextVitals from 'eslint-config-next/core-web-vitals'
-import tseslint from 'typescript-eslint';
-import tsParser from "@typescript-eslint/parser";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-    ...nextVitals,
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    globalIgnores([
-        'node_modules/**',
-        '.next/**',
-        'public/**',
-        'out/**',
-        'next-env.d.ts',
-        'tailwind.config.js',
-        'postcss.config.cjs',
-        '.lintstagedrc.js'
-    ]),
-    {
-        files: ["**/*.{ts,tsx}"],
-        languageOptions: {
-        parser: tsParser,
-        parserOptions: {
-            ecmaVersion: "latest",
-            sourceType: "module",
-        },
-        },
+  ...nextVitals,
+  globalIgnores([
+    ".next/**",
+    "docs/**",
+    "node_modules/**",
+    "out/**",
+    "public/**",
+    "next-env.d.ts",
+  ]),
+  {
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
     },
-    {
-        rules: {
-            'react/react-in-jsx-scope': 'off',
-            'react/no-unknown-property': 'off',
-            '@typescript-eslint/no-explicit-any': 'warn'
-        }
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/no-unknown-property": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
+  },
 ]);
